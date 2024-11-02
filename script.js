9dea113d1c18c255cbc1ea6680405ad5ee871e20
TIKENDRA DESHMUKH <tikendradeshmukhid@gmail.com>
2024-11-02 07:03:05 +0000

    5

--- script.js
+++ script.js
@@ -119,7 +119,7 @@ const emoji_number = [
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-it's        [array[i], array[j]] = [array[j], array[i]];
+        [array[i], array[j]] = [array[j], array[i]];
     }
 }
 
